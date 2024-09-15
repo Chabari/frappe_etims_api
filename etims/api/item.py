77b5422c24e821eba373c8ad9ba187b5e2debd2c
Dev <mukundichabari@gmail.com>
2024-09-15 11:24:49 +0300

    tax

--- etims/api/item.py
+++ etims/api/item.py
@@ -57,7 +57,7 @@ def sync_items():
     items = frappe.db.sql("""
         SELECT name
         FROM `tabItem`
-        WHERE custom_etims_item_code IS NULL LIMIT 100
+        WHERE custom_etims_item_code IS NULL LIMIT 200
     """)
     for itm in items:
         doc = frappe.get_doc('Item', itm)
